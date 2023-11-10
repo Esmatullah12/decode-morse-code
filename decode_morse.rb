2d0605b8d90896661220b5e2056a4f9928fe5f12
Esmatullah <esmatqadeemi12@gmail.com>
2023-11-10 09:21:59 +0100

    solve the decdoe morse challenge

---
+++ decode_morse.rb
@@ -0,0 +1,62 @@
+$morseSymbol = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '-----' => '0',
+  '.----' => 1,
+  '..---' => 2,
+  '...--' => 3,
+  '....-' => 4,
+  '.....' => 5,
+  '-....' => 6,
+  '--...' => 7,
+  '---..' => 8,
+  '----.' => 9
+};
+
+def decodeChar(morseChar)
+    return $morseSymbol[morseChar]
+end
+
+def decodeWord(morseWords)
+    @decodeWords = ""
+    charachters = morseWords.split(' ')
+    charachters.each do |n|
+      @decodeWords += decodeChar(n)
+    end
+    return @decodeWords
+end
+
+def decodeMessage(morseMessage)
+    @decodeMessage = ""
+    messageWords = morseMessage.split('   ')
+    messageWords.each do |n|
+      @decodeMessage += decodeWord(n)
+    end
+    print @decodeMessage
+end
+
+decodeWord(".- -... --- -..- ..-. ..- .-.. .-.. --- ..-. .-. ..- -... .. . ...")
